a6894bf0de0a8f9a43e0d5712a9e33b71a3b701d
Joel Wetzel <jwetzel@affirma.com>
2024-02-06 11:23:43 -0600

    Adds unit tests for binding fans.

--- tests/SpeedTests.groovy
+++ tests/SpeedTests.groovy
@@ -2,6 +2,7 @@ package joelwetzel.switch_bindings.tests
 
 import me.biocomp.hubitat_ci.util.device_fixtures.SwitchFixtureFactory
 import me.biocomp.hubitat_ci.util.device_fixtures.DimmerFixtureFactory
+import me.biocomp.hubitat_ci.util.device_fixtures.FanFixtureFactory
 import me.biocomp.hubitat_ci.util.IntegrationAppExecutor
 
 import me.biocomp.hubitat_ci.api.app_api.AppExecutor
@@ -17,7 +18,7 @@ import me.biocomp.hubitat_ci.validation.Flags
 import spock.lang.Specification
 
 /**
-* Level tests for SwitchBindingInstance.groovy
+* Speed tests for SwitchBindingInstance.groovy
 */
 class SpeedTests extends Specification {
     private HubitatAppSandbox sandbox = new HubitatAppSandbox(new File('SwitchBindingInstance.groovy'))
@@ -39,222 +40,95 @@ class SpeedTests extends Specification {
     }
 
     def switchFixture1 = SwitchFixtureFactory.create('s1')
-
     def dimmerFixture1 = DimmerFixtureFactory.create('d1')
-    def dimmerFixture2 = DimmerFixtureFactory.create('d2')
-    def dimmerFixture3 = DimmerFixtureFactory.create('d3')
+    def fanFixture1 = FanFixtureFactory.create('f1')
+    def fanFixture2 = FanFixtureFactory.create('f2')
 
-    def switches = [switchFixture1, dimmerFixture1, dimmerFixture2, dimmerFixture3]
-
-    def appScript = sandbox.run(api: appExecutor,
-        validationFlags: [Flags.AllowAnyExistingDeviceAttributeOrCapabilityInSubscribe],
-        userSettingValues: [nameOverride: "Custom Name", switches: switches, masterSwitchId: null, masterOnly: false, pollMaster: false, pollingInterval: 5, responseTime: 5000, enableLogging: true])
-
-    def setup() {
-        throw new Exception("Not implemented")
-
-        appExecutor.setSubscribingScript(appScript)
-        appScript.initialize()
-    }
-
-    def cleanup() {
-    }
-
-    void "setLevel on one dimmer affects the others"() {
+    void "Two bound fans turn on"() {
         given:
-        switchFixture1.initialize(appExecutor, [switch: "off"])
-        dimmerFixture1.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture2.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture3.initialize(appExecutor, [switch: "off", level: 50])
-
-        when:
-        dimmerFixture1.setLevel(100)
-
-        then:
-        appAtomicState.controllingDeviceId == dimmerFixture1.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 100
-        dimmerFixture2.state.level == 100
-        dimmerFixture3.state.level == 100
-    }
-
-    void "Switching one dimmer off affects the others, but not level"() {
-        given:
-        switchFixture1.initialize(appExecutor, [switch: "on"])
-        dimmerFixture1.initialize(appExecutor, [switch: "on", level: 50])
-        dimmerFixture2.initialize(appExecutor, [switch: "on", level: 50])
-        dimmerFixture3.initialize(appExecutor, [switch: "on", level: 50])
-
-        when:
-        dimmerFixture2.off()
-
-        then:
-        appAtomicState.controllingDeviceId == dimmerFixture2.deviceId
-        switchFixture1.state.switch == "off"
-        dimmerFixture1.state.switch == "off"
-        dimmerFixture2.state.switch == "off"
-        dimmerFixture3.state.switch == "off"
-        dimmerFixture1.state.level == 50
-        dimmerFixture2.state.level == 50
-        dimmerFixture3.state.level == 50
-    }
-
-    void "Can handle inconsistent initial state"() {
-        given:
-        switchFixture1.initialize(appExecutor, [switch: "off"])
-        dimmerFixture1.initialize(appExecutor, [switch: "on", level: 5])
-        dimmerFixture2.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture3.initialize(appExecutor, [switch: "off", level: 100])
-
-        when:
-        dimmerFixture2.setLevel(80)
-
-        then:
-        appAtomicState.controllingDeviceId == dimmerFixture2.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 80
-        dimmerFixture2.state.level == 80
-        dimmerFixture3.state.level == 80
-    }
-
-    void "setLevel can turn a switch on, even if the dimmer was already on"() {
-        given:
-        switchFixture1.initialize(appExecutor, [switch: "off"])
-        dimmerFixture1.initialize(appExecutor, [switch: "on", level: 5])
-        dimmerFixture2.initialize(appExecutor, [switch: "on", level: 50])
-        dimmerFixture3.initialize(appExecutor, [switch: "on", level: 100])
-
-        when:
-        dimmerFixture2.setLevel(80)
-
-        then:
-        appAtomicState.controllingDeviceId == dimmerFixture2.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 80
-        dimmerFixture2.state.level == 80
-        dimmerFixture3.state.level == 80
-    }
-
-    void "There's a response time, within which we do not propagate signals from other than the controlling device"() {
-        given:
-        switchFixture1.initialize(appExecutor, [switch: "off"])
-        dimmerFixture1.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture2.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture3.initialize(appExecutor, [switch: "off", level: 50])
-
-        when:
-        dimmerFixture1.setLevel(80)
-
-        then:
-        appAtomicState.controllingDeviceId == dimmerFixture1.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 80
-        dimmerFixture2.state.level == 80
-        dimmerFixture3.state.level == 80
-
-        when:
-        TimeKeeper.advanceSeconds(1)
+            def appScript = sandbox.run(api: appExecutor,
+                validationFlags: [Flags.AllowAnyExistingDeviceAttributeOrCapabilityInSubscribe],
+                userSettingValues: [nameOverride: "Custom Name", switches: [fanFixture1, fanFixture2], masterSwitchId: null, masterOnly: false, pollMaster: false, pollingInterval: 5, responseTime: 5000, enableLogging: true])
+            appExecutor.setSubscribingScript(appScript)
+            appScript.initialize()
 
         and:
-        dimmerFixture2.setLevel(20)
+            fanFixture1.initialize(appExecutor, [switch: "off", level: 0, speed: "off"])
+            fanFixture2.initialize(appExecutor, [switch: "off", level: 0, speed: "off"])
+
+        when:
+            fanFixture1.on()
 
         then:
-        1 * log.debug("checkForFeedbackLoop: Preventing feedback loop")
-        appAtomicState.controllingDeviceId == dimmerFixture1.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 80
-        dimmerFixture2.state.level == 20
-        dimmerFixture3.state.level == 80
+            fanFixture1.state.switch == "on"
+            fanFixture2.state.switch == "on"
+            fanFixture1.state.level == 100
+            fanFixture2.state.level == 100
+            fanFixture1.state.speed == "high"
+            fanFixture2.state.speed == "high"
     }
 
-    void "Response time applies even for mixed commands"() {
+    void "Two bound fans change speed"() {
         given:
-        switchFixture1.initialize(appExecutor, [switch: "off"])
-        dimmerFixture1.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture2.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture3.initialize(appExecutor, [switch: "off", level: 50])
-
-        when:
-        dimmerFixture1.setLevel(80)
-
-        then:
-        appAtomicState.controllingDeviceId == dimmerFixture1.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 80
-        dimmerFixture2.state.level == 80
-        dimmerFixture3.state.level == 80
-
-        when:
-        TimeKeeper.advanceSeconds(1)
+            def appScript = sandbox.run(api: appExecutor,
+                validationFlags: [Flags.AllowAnyExistingDeviceAttributeOrCapabilityInSubscribe],
+                userSettingValues: [nameOverride: "Custom Name", switches: [fanFixture1, fanFixture2], masterSwitchId: null, masterOnly: false, pollMaster: false, pollingInterval: 5, responseTime: 5000, enableLogging: true])
+            appExecutor.setSubscribingScript(appScript)
+            appScript.initialize()
 
         and:
-        dimmerFixture2.off()
+            fanFixture1.initialize(appExecutor, [switch: "on", level: 100, speed: "high"])
+            fanFixture2.initialize(appExecutor, [switch: "on", level: 100, speed: "high"])
+
+        when:
+            fanFixture1.setSpeed("low")
 
         then:
-        1 * log.debug("checkForFeedbackLoop: Preventing feedback loop")
-        appAtomicState.controllingDeviceId == dimmerFixture1.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "off"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 80
-        dimmerFixture2.state.level == 80
-        dimmerFixture3.state.level == 80
+            fanFixture1.state.switch == "on"
+            fanFixture1.state.speed == "low"
+            fanFixture1.state.level == 16
+            fanFixture2.state.switch == "on"
+            fanFixture2.state.speed == "low"
+            fanFixture2.state.level == 16
     }
 
-    void "After the responseTime, other dimmers can become the controlling device"() {
+    void "A bound switch can turn a fan on"() {
         given:
-        switchFixture1.initialize(appExecutor, [switch: "off"])
-        dimmerFixture1.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture2.initialize(appExecutor, [switch: "off", level: 50])
-        dimmerFixture3.initialize(appExecutor, [switch: "off", level: 50])
-
-        when:
-        dimmerFixture1.setLevel(80)
-
-        then:
-        appAtomicState.controllingDeviceId == dimmerFixture1.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 80
-        dimmerFixture2.state.level == 80
-        dimmerFixture3.state.level == 80
-
-        when:
-        TimeKeeper.advanceSeconds(5)
+            def appScript = sandbox.run(api: appExecutor,
+                validationFlags: [Flags.AllowAnyExistingDeviceAttributeOrCapabilityInSubscribe],
+                userSettingValues: [nameOverride: "Custom Name", switches: [fanFixture1, switchFixture1], masterSwitchId: null, masterOnly: false, pollMaster: false, pollingInterval: 5, responseTime: 5000, enableLogging: true])
+            appExecutor.setSubscribingScript(appScript)
+            appScript.initialize()
 
         and:
-        dimmerFixture2.setLevel(20)
+            fanFixture1.initialize(appExecutor, [switch: "off", level: 0, speed: "off"])
+            switchFixture1.initialize(appExecutor, [switch: "off"])
+
+        when:
+            switchFixture1.on()
 
         then:
-        appAtomicState.controllingDeviceId == dimmerFixture2.deviceId
-        switchFixture1.state.switch == "on"
-        dimmerFixture1.state.switch == "on"
-        dimmerFixture2.state.switch == "on"
-        dimmerFixture3.state.switch == "on"
-        dimmerFixture1.state.level == 20
-        dimmerFixture2.state.level == 20
-        dimmerFixture3.state.level == 20
+            fanFixture1.state.switch == "on"
+            fanFixture1.state.level == 100
+            fanFixture1.state.speed == "high"
     }
 
+    void "A bound fan can turn a switch off"() {
+        given:
+            def appScript = sandbox.run(api: appExecutor,
+                validationFlags: [Flags.AllowAnyExistingDeviceAttributeOrCapabilityInSubscribe],
+                userSettingValues: [nameOverride: "Custom Name", switches: [fanFixture1, switchFixture1], masterSwitchId: null, masterOnly: false, pollMaster: false, pollingInterval: 5, responseTime: 5000, enableLogging: true])
+            appExecutor.setSubscribingScript(appScript)
+            appScript.initialize()
+
+        and:
+            fanFixture1.initialize(appExecutor, [switch: "on", level: 100, speed: "high"])
+            switchFixture1.initialize(appExecutor, [switch: "on"])
+
+        when:
+            fanFixture1.off()
+
+        then:
+            switchFixture1.state.switch == "off"
+    }
 }
